63b8fb1cece8718635b37fc5f1eef7728adce3ba
aalmeida00 <andre-silva78@hotmail.com>
2021-05-18 22:59:20 -0300

    feat: compress css and export sass function to compile using module export sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,10 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 
 function compileSass() {
-  return gulp.src('css/scss/**/*.scss').pipe(sass()).pipe(gulp.dest('css/'));
+  return gulp
+    .src('css/scss/**/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(gulp.dest('css/'));
 }
+
+exports.sass = compileSass;
