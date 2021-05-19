3182103e6afcc4d03d890c7023f3401f2496831b
aalmeida00 <andre-silva78@hotmail.com>
2021-05-18 23:14:45 -0300

    fix: gulp autoprefix need to change some configs

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 function compileSass() {
   return gulp
     .src('css/scss/**/*.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(autoprefixer({ cascade: false, grid: 'autoplace' }))
     .pipe(gulp.dest('css/'));
 }
 
