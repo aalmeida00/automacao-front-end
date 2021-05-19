4c60dc22c370ad28ae8acfe3911e3c3ac09f3ffb
aalmeida00 <andre-silva78@hotmail.com>
2021-05-18 23:35:56 -0300

    feat: gulp routine to use browserSync and create a local server

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,24 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
 
 function compileSass() {
   return gulp
     .src('css/scss/**/*.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(autoprefixer({ cascade: false, grid: 'autoplace' }))
-    .pipe(gulp.dest('css/'));
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.stream());
 }
 
 function keepWatching() {
+  browserSync.init({
+    server: {
+      baseDir: './',
+    },
+  });
+
   gulp.watch('css/scss/*.scss', compileSass);
 }
 
