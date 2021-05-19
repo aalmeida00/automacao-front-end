bb459d43bcb9a51fb0d277894eb4bf5552b165e8
aalmeida00 <andre-silva78@hotmail.com>
2021-05-18 23:46:01 -0300

    chore: comments on archive

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
+// Adiciona os modulos instalados
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 
+// Função para compilar o sass e adicionar os prefixos
 function compileSass() {
   return gulp
     .src('css/scss/**/*.scss')
@@ -12,15 +14,16 @@ function compileSass() {
     .pipe(browserSync.stream());
 }
 
+// Função para fazer o browser sync e continuar dando watch em arquivos css/html
 function keepWatching() {
   browserSync.init({
     server: {
       baseDir: './',
     },
   });
-
   gulp.watch('css/scss/*.scss', compileSass);
   gulp.watch('*.html').on('change', browserSync.reload);
 }
 
+// Exporta a função para ser chamada como gulp:watch
 exports.watch = keepWatching;
